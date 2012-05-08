e30b8b5062d5f2b99b528c9a49d085971ec778c9
Matt Urbanski <matt@iflowfor8hours.info>
2012-05-08 14:19:51 -0400

    fixed linking script

--- symlinkit.sh
+++ symlinkit.sh
@@ -2,9 +2,9 @@
 
 cd ..
 
-ln -sn dotfiles/.bash .bash
-ln -sn dotfiles/.bashrc .bashrc
-ln -sn dotfiles/.bash_profile .bash_profile
-ln -sn dotfiles/.vim .vim
-ln -sn dotfiles/.vimrc .vimrc
-ln -sn dotfiles/.irssi .irssi
+ln -sn dotfiles/.bash ~/.bash
+ln -sn dotfiles/.bashrc ~/.bashrc
+ln -sn dotfiles/.bash_profile ~/.bash_profile
+ln -sn dotfiles/.vim ~/.vim
+ln -sn dotfiles/.vimrc ~/.vimrc
+ln -sn dotfiles/.irssi ~/.irssi
