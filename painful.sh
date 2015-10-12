6b2cdd3f3429f70dd16c2ae245e5612269bd459a
matt urbanski <matt.urbanski@iflowfor8hours.info>
2015-10-11 18:21:40 -0700

    minor painful changes

--- painful.sh
+++ painful.sh
@@ -11,6 +11,7 @@ sudo apt-get update
 sudo apt-get install mc gnome-commander git vim zsh htop kupfer libgnutls-dev build-essential cmake libuuid1 libuu-dev libuu0 uuid-dev terminator autojump mutt-patched exuberant-ctags offlineimap redshift sqlite notmuch-mutt
 
 # Taskwarrior
+mkdir -p /home/matt/.task/
 mkdir -p /tmp/taskinstaller
 cd /tmp/taskinstaller
 curl -O http://taskwarrior.org/download/task-2.4.4.tar.gz
@@ -20,9 +21,10 @@ cmake -DCMAKE_BUILD_TYPE=release .
 make
 sudo make install
 
-mkdir -p /home/matt/.task/
+
 
 # rbenv
+cd ~
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
@@ -36,4 +38,3 @@ mkdir -p /home/matt/Mail/.offlineimap
 mkdir -p ~/.config/offlineimap/
 touch ~/.config/offlineimap/matt.iflowfor8hours.info
 echo "Add your password to ~/.config/offlineimap/matt.iflowfor8hours.info"
-
